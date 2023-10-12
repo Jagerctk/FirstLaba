22be8506998d130eceec65de955698842284e0dc
Jagerctk <146988638+Jagerctk@users.noreply.github.com>
2023-10-12 18:18:59 +0300

    Add files via upload

---
+++ Лабораторная работа №1/Разделение игроков.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+a = len(list_players)
+team1 = list_players[0:3]
+team2 = list_players[3:7]
+# TODO Разделите участников на две команды
+print(team1)
+print(team2)
